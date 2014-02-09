796c6c9264d3c9bad294194a59152e0f39770048
Dennis Kaselow <dennis.kaselow+github@gmail.com>
2014-02-09 14:26:26 +0100

    updated for current version of dart

--- web/playground_dart_webgl.dart
+++ web/playground_dart_webgl.dart
@@ -1,4 +1,6 @@
-#import("dart:html");
+import 'dart:html';
+import 'dart:web_gl';
+import 'dart:typed_data';
 
 /**
  * Links:
@@ -12,20 +14,20 @@
 
 void main() {
 
-  CanvasElement canvas = query('canvas');
+  CanvasElement canvas = querySelector('canvas');
 
-  AnchorElement link = query('a#lesson001');
-  link.on.click.add((e) {
+  AnchorElement link = querySelector('a#lesson001');
+  link.onClick.listen((e) {
     Lesson lesson = new Lesson001(canvas);
     lesson.start();
   });
-  link = query('a#lesson002');
-  link.on.click.add((e) {
+  link = querySelector('a#lesson002');
+  link.onClick.listen((e) {
     Lesson lesson = new Lesson002(canvas);
     lesson.start();
   });
-  link = query('a#lesson003');
-  link.on.click.add((e) {
+  link = querySelector('a#lesson003');
+  link.onClick.listen((e) {
     Lesson lesson = new Lesson003(canvas);
     lesson.start();
   });
@@ -44,18 +46,14 @@ class Lesson001 implements Lesson {
   Lesson001(this.canvas);
 
   void start() {
-    canvas.parent.rect.then((ElementRect rect) {
-      canvas.width = 800;
-      canvas.height = 800;
+    canvas.width = 800;
+    canvas.height = 800;
 
-      WebGLRenderingContext gl = canvas.getContext('experimental-webgl');
+    RenderingContext gl = canvas.getContext3d();
 
-      gl.viewport(0, 0, canvas.width, canvas.height);
-      gl.clearColor(0, 0, 0, 1);
-      gl.clear(WebGLRenderingContext.COLOR_BUFFER_BIT);
-
-
-    });
+    gl.viewport(0, 0, canvas.width, canvas.height);
+    gl.clearColor(0, 0, 0, 1);
+    gl.clear(RenderingContext.COLOR_BUFFER_BIT);
   }
 }
 
@@ -69,95 +67,93 @@ class Lesson002 implements Lesson  {
   final String colorVariable = "uColor";
 
   void start() {
-    canvas.parent.rect.then((ElementRect rect) {
-      canvas.width = 800;
-      canvas.height = 800;
+    canvas.width = 800;
+    canvas.height = 800;
 
-      WebGLRenderingContext gl = canvas.getContext('experimental-webgl');
+    RenderingContext gl = canvas.getContext3d();
 
-      gl.viewport(0, 0, canvas.width, canvas.height);
-      gl.clearColor(0, 0, 0, 1);
-      gl.clear(WebGLRenderingContext.COLOR_BUFFER_BIT);
+    gl.viewport(0, 0, canvas.width, canvas.height);
+    gl.clearColor(0, 0, 0, 1);
+    gl.clear(RenderingContext.COLOR_BUFFER_BIT);
 
-      String vertex = """
-        attribute vec2 $attrVertexPosition;
-          
-        void main() {
-          mat4 view = mat4( vec4(1, 0, 0, 0),
-                            vec4(0, 1, 0, 0),
-                            vec4(0, 0, 1, 0),
-                            vec4(0, 0, 0, 1));
-          mat4 world = mat4(vec4(1, 0, 0, 0),
-                            vec4(0, 1, 0, 0),
-                            vec4(0, 0, 1, 0),
-                            vec4(0, 0, 0, 1));
-          mat4 worldViewProjection = view * world;
-          gl_Position = (worldViewProjection * vec4($attrVertexPosition, 0.0, 1.0));
-        }
-      """;
-
-      WebGLShader vs = gl.createShader(WebGLRenderingContext.VERTEX_SHADER);
-      gl.shaderSource(vs, vertex);
-      gl.compileShader(vs);
-
-      String fragment = """
-        #ifdef GL_ES
-        precision highp float;
-        #endif
-          
-        uniform vec4 $colorVariable;
-          
-        void main() {
-          gl_FragColor = $colorVariable;
-        }
-      """;
-
-      WebGLShader fs = gl.createShader(WebGLRenderingContext.FRAGMENT_SHADER);
-      gl.shaderSource(fs, fragment);
-      gl.compileShader(fs);
-
-      WebGLProgram program = gl.createProgram();
-      gl.attachShader(program, vs);
-      gl.attachShader(program, fs);
-      gl.linkProgram(program);
-
-      if (!gl.getShaderParameter(vs, WebGLRenderingContext.COMPILE_STATUS)) {
-        print(gl.getShaderInfoLog(vs));
+    String vertex = """
+      attribute vec2 $attrVertexPosition;
+        
+      void main() {
+        mat4 view = mat4( vec4(1, 0, 0, 0),
+                          vec4(0, 1, 0, 0),
+                          vec4(0, 0, 1, 0),
+                          vec4(0, 0, 0, 1));
+        mat4 world = mat4(vec4(1, 0, 0, 0),
+                          vec4(0, 1, 0, 0),
+                          vec4(0, 0, 1, 0),
+                          vec4(0, 0, 0, 1));
+        mat4 worldViewProjection = view * world;
+        gl_Position = (worldViewProjection * vec4($attrVertexPosition, 0.0, 1.0));
       }
+    """;
 
-      if (!gl.getShaderParameter(fs, WebGLRenderingContext.COMPILE_STATUS)) {
-        print(gl.getShaderInfoLog(fs));
+    Shader vs = gl.createShader(RenderingContext.VERTEX_SHADER);
+    gl.shaderSource(vs, vertex);
+    gl.compileShader(vs);
+
+    String fragment = """
+      #ifdef GL_ES
+      precision highp float;
+      #endif
+        
+      uniform vec4 $colorVariable;
+        
+      void main() {
+        gl_FragColor = $colorVariable;
       }
+    """;
 
-      if (!gl.getProgramParameter(program, WebGLRenderingContext.LINK_STATUS)) {
-        print(gl.getProgramInfoLog(program));
-      }
+    Shader fs = gl.createShader(RenderingContext.FRAGMENT_SHADER);
+    gl.shaderSource(fs, fragment);
+    gl.compileShader(fs);
+
+    Program program = gl.createProgram();
+    gl.attachShader(program, vs);
+    gl.attachShader(program, fs);
+    gl.linkProgram(program);
+
+    if (!gl.getShaderParameter(vs, RenderingContext.COMPILE_STATUS)) {
+      print(gl.getShaderInfoLog(vs));
+    }
+
+    if (!gl.getShaderParameter(fs, RenderingContext.COMPILE_STATUS)) {
+      print(gl.getShaderInfoLog(fs));
+    }
+
+    if (!gl.getProgramParameter(program, RenderingContext.LINK_STATUS)) {
+      print(gl.getProgramInfoLog(program));
+    }
 
 
-      Float32Array vertices = new Float32Array.fromList(
-          [
-           0.5, 0.5, -0.5, 0.5, 0.5, -0.5, // Triangle 1
-           -0.5, 0.5, 0.5, -0.5, -0.5, -0.5 // Triangle 2
-          ]);
+    Float32List vertices = new Float32List.fromList(
+        [
+         0.5, 0.5, -0.5, 0.5, 0.5, -0.5, // Triangle 1
+         -0.5, 0.5, 0.5, -0.5, -0.5, -0.5 // Triangle 2
+        ]);
 
-      WebGLBuffer squareBuffer = gl.createBuffer();
-      gl.bindBuffer(WebGLRenderingContext.ARRAY_BUFFER, squareBuffer);
-      gl.bufferData(WebGLRenderingContext.ARRAY_BUFFER, vertices, WebGLRenderingContext.STATIC_DRAW);
+    Buffer squareBuffer = gl.createBuffer();
+    gl.bindBuffer(RenderingContext.ARRAY_BUFFER, squareBuffer);
+    gl.bufferData(RenderingContext.ARRAY_BUFFER, vertices, RenderingContext.STATIC_DRAW);
 
-      int itemSize = 2;
-      int numItems = vertices.length ~/ itemSize;
+    int itemSize = 2;
+    int numItems = vertices.length ~/ itemSize;
 
-      gl.useProgram(program);
+    gl.useProgram(program);
 
-      WebGLUniformLocation uColor = gl.getUniformLocation(program, colorVariable);
-      gl.uniform4fv(uColor, new Float32Array.fromList([0.9, 0.3, 0.0, 0.5]));
+    UniformLocation uColor = gl.getUniformLocation(program, colorVariable);
+    gl.uniform4fv(uColor, new Float32List.fromList([0.9, 0.3, 0.0, 0.5]));
 
-      int aVertextPosition = gl.getAttribLocation(program, attrVertexPosition);
-      gl.enableVertexAttribArray(aVertextPosition);
-      gl.vertexAttribPointer(aVertextPosition, itemSize, WebGLRenderingContext.FLOAT, false, 0, 0);
+    int aVertextPosition = gl.getAttribLocation(program, attrVertexPosition);
+    gl.enableVertexAttribArray(aVertextPosition);
+    gl.vertexAttribPointer(aVertextPosition, itemSize, RenderingContext.FLOAT, false, 0, 0);
 
-      gl.drawArrays(WebGLRenderingContext.TRIANGLES, 0, numItems);
-    });
+    gl.drawArrays(RenderingContext.TRIANGLES, 0, numItems);
   }
 
 }
@@ -172,85 +168,83 @@ class Lesson003 implements Lesson  {
   final String colorVariable = "uColor";
 
   void start() {
-    canvas.parent.rect.then((ElementRect rect) {
-      canvas.width = 800;
-      canvas.height = 800;
+    canvas.width = 800;
+    canvas.height = 800;
 
-      WebGLRenderingContext gl = canvas.getContext('experimental-webgl');
+    RenderingContext gl = canvas.getContext3d();
 
-      gl.viewport(0, 0, canvas.width, canvas.height);
-      gl.clearColor(0, 0.5, 0, 1);
-      gl.clear(WebGLRenderingContext.COLOR_BUFFER_BIT);
+    gl.viewport(0, 0, canvas.width, canvas.height);
+    gl.clearColor(0, 0.5, 0, 1);
+    gl.clear(RenderingContext.COLOR_BUFFER_BIT);
 
-      String vertex = """
-        attribute vec2 $vertexPositionAttribute;
-          
-        void main() {
-          gl_Position = vec4($vertexPositionAttribute, 0.0, 1.0);
-        }
-      """;
-
-      WebGLShader vs = gl.createShader(WebGLRenderingContext.VERTEX_SHADER);
-      gl.shaderSource(vs, vertex);
-      gl.compileShader(vs);
-
-      String fragment = """
-        #ifdef GL_ES
-        precision highp float;
-        #endif
-          
-        uniform vec4 $colorVariable;
-          
-        void main() {
-          gl_FragColor = $colorVariable;
-        }
-      """;
-
-      WebGLShader fs = gl.createShader(WebGLRenderingContext.FRAGMENT_SHADER);
-      gl.shaderSource(fs, fragment);
-      gl.compileShader(fs);
-
-      WebGLProgram program = gl.createProgram();
-      gl.attachShader(program, vs);
-      gl.attachShader(program, fs);
-      gl.linkProgram(program);
-
-      if (!gl.getShaderParameter(vs, WebGLRenderingContext.COMPILE_STATUS)) {
-        print(gl.getShaderInfoLog(vs));
+    String vertex = """
+      attribute vec2 $vertexPositionAttribute;
+        
+      void main() {
+        gl_Position = vec4($vertexPositionAttribute, 0.0, 1.0);
       }
+    """;
 
-      if (!gl.getShaderParameter(fs, WebGLRenderingContext.COMPILE_STATUS)) {
-        print(gl.getShaderInfoLog(fs));
+    Shader vs = gl.createShader(RenderingContext.VERTEX_SHADER);
+    gl.shaderSource(vs, vertex);
+    gl.compileShader(vs);
+
+    String fragment = """
+      #ifdef GL_ES
+      precision highp float;
+      #endif
+        
+      uniform vec4 $colorVariable;
+        
+      void main() {
+        gl_FragColor = $colorVariable;
       }
+    """;
 
-      if (!gl.getProgramParameter(program, WebGLRenderingContext.LINK_STATUS)) {
-        print(gl.getProgramInfoLog(program));
-      }
+    Shader fs = gl.createShader(RenderingContext.FRAGMENT_SHADER);
+    gl.shaderSource(fs, fragment);
+    gl.compileShader(fs);
 
-      Float32Array vertices = new Float32Array.fromList(
-          [
-           -0.4, 0.6, 0.6, 0.6, 0.6, -0.4, // Triangle 1
-           -0.6, 0.4, 0.4, -0.6, -0.6, -0.6 // Triangle 2
-          ]);
+    Program program = gl.createProgram();
+    gl.attachShader(program, vs);
+    gl.attachShader(program, fs);
+    gl.linkProgram(program);
 
-      WebGLBuffer vbuffer = gl.createBuffer();
-      gl.bindBuffer(WebGLRenderingContext.ARRAY_BUFFER, vbuffer);
-      gl.bufferData(WebGLRenderingContext.ARRAY_BUFFER, vertices, WebGLRenderingContext.STATIC_DRAW);
+    if (!gl.getShaderParameter(vs, RenderingContext.COMPILE_STATUS)) {
+      print(gl.getShaderInfoLog(vs));
+    }
 
-      int itemSize = 2;
-      int numItems = vertices.length ~/ itemSize;
+    if (!gl.getShaderParameter(fs, RenderingContext.COMPILE_STATUS)) {
+      print(gl.getShaderInfoLog(fs));
+    }
 
-      gl.useProgram(program);
+    if (!gl.getProgramParameter(program, RenderingContext.LINK_STATUS)) {
+      print(gl.getProgramInfoLog(program));
+    }
 
-      WebGLUniformLocation uColor = gl.getUniformLocation(program, colorVariable);
-      gl.uniform4fv(uColor, new Float32Array.fromList([0.9, 0.3, 0.0, 0.5]));
+    Float32List vertices = new Float32List.fromList(
+        [
+         -0.4, 0.6, 0.6, 0.6, 0.6, -0.4, // Triangle 1
+         -0.6, 0.4, 0.4, -0.6, -0.6, -0.6 // Triangle 2
+        ]);
 
-      int aVertextPosition = gl.getAttribLocation(program, vertexPositionAttribute);
-      gl.enableVertexAttribArray(aVertextPosition);
-      gl.vertexAttribPointer(aVertextPosition, itemSize, WebGLRenderingContext.FLOAT, false, 0, 0);
+    Buffer vbuffer = gl.createBuffer();
+    gl.bindBuffer(RenderingContext.ARRAY_BUFFER, vbuffer);
+    gl.bufferData(RenderingContext.ARRAY_BUFFER, vertices, RenderingContext.STATIC_DRAW);
 
-      gl.drawArrays(WebGLRenderingContext.TRIANGLES, 0, numItems);
-    });
+    int itemSize = 2;
+    int numItems = vertices.length ~/ itemSize;
+
+    gl.useProgram(program);
+
+    UniformLocation uColor = gl.getUniformLocation(program, colorVariable);
+    gl.uniform4fv(uColor, new Float32List.fromList([0.9, 0.3, 0.0, 0.5]));
+
+    int aVertextPosition = gl.getAttribLocation(program, vertexPositionAttribute);
+    gl.enableVertexAttribArray(aVertextPosition);
+    gl.vertexAttribPointer(aVertextPosition, itemSize, RenderingContext.FLOAT, false, 0, 0);
+
+    gl.drawArrays(RenderingContext.TRIANGLES, 0, numItems);
   }
 }
 
@@ -265,98 +259,96 @@ class Lesson004 implements Lesson  {
   final String uColor = "uColor";
 
   void start() {
-    canvas.parent.rect.then((ElementRect rect) {
-      canvas.width = 800;
-      canvas.height = 800;
+    canvas.width = 800;
+    canvas.height = 800;
 
-      WebGLRenderingContext gl = canvas.getContext('experimental-webgl');
+    RenderingContext gl = canvas.getContext3d();
 
-      gl.viewport(0, 0, canvas.width, canvas.height);
-      gl.clearColor(0, 0, 0, 1);
-      gl.clear(WebGLRenderingContext.COLOR_BUFFER_BIT);
+    gl.viewport(0, 0, canvas.width, canvas.height);
+    gl.clearColor(0, 0, 0, 1);
+    gl.clear(RenderingContext.COLOR_BUFFER_BIT);
 
-      String vertex = """
-        attribute vec2 $attrVertex;
-        uniform vec3 $uPosition;
-          
-        void main() {
-          mat4 pMatrix = mat4(vec4(1, 0, 0, 0),
-                              vec4(0, 1, 0, 0),
-                              vec4(0, 0, -1, -1),
-                              vec4(0, 0, -0.2, 0));
-          mat4 mvMatrix = mat4(vec4(1, 0, 0, 0),
-                               vec4(0, 1, 0, 0),
-                               vec4(0, 0, 1, 0),
-                               vec4(vec3($uPosition), 1));
-          gl_Position = pMatrix * mvMatrix * vec4($attrVertex, 0, 1.0);
-        }
-      """;
-
-      WebGLShader vs = gl.createShader(WebGLRenderingContext.VERTEX_SHADER);
-      gl.shaderSource(vs, vertex);
-      gl.compileShader(vs);
-
-      String fragment = """
-        #ifdef GL_ES
-        precision highp float;
-        #endif
-          
-        uniform vec4 $uColor;
-          
-        void main() {
-          gl_FragColor = $uColor;
-        }
-      """;
-
-      WebGLShader fs = gl.createShader(WebGLRenderingContext.FRAGMENT_SHADER);
-      gl.shaderSource(fs, fragment);
-      gl.compileShader(fs);
-
-      WebGLProgram program = gl.createProgram();
-      gl.attachShader(program, vs);
-      gl.attachShader(program, fs);
-      gl.linkProgram(program);
-
-      if (!gl.getShaderParameter(vs, WebGLRenderingContext.COMPILE_STATUS)) {
-        print(gl.getShaderInfoLog(vs));
+    String vertex = """
+      attribute vec2 $attrVertex;
+      uniform vec3 $uPosition;
+        
+      void main() {
+        mat4 pMatrix = mat4(vec4(1, 0, 0, 0),
+                            vec4(0, 1, 0, 0),
+                            vec4(0, 0, -1, -1),
+                            vec4(0, 0, -0.2, 0));
+        mat4 mvMatrix = mat4(vec4(1, 0, 0, 0),
+                             vec4(0, 1, 0, 0),
+                             vec4(0, 0, 1, 0),
+                             vec4(vec3($uPosition), 1));
+        gl_Position = pMatrix * mvMatrix * vec4($attrVertex, 0, 1.0);
       }
+    """;
 
-      if (!gl.getShaderParameter(fs, WebGLRenderingContext.COMPILE_STATUS)) {
-        print(gl.getShaderInfoLog(fs));
+    Shader vs = gl.createShader(RenderingContext.VERTEX_SHADER);
+    gl.shaderSource(vs, vertex);
+    gl.compileShader(vs);
+
+    String fragment = """
+      #ifdef GL_ES
+      precision highp float;
+      #endif
+        
+      uniform vec4 $uColor;
+        
+      void main() {
+        gl_FragColor = $uColor;
       }
+    """;
 
-      if (!gl.getProgramParameter(program, WebGLRenderingContext.LINK_STATUS)) {
-        print(gl.getProgramInfoLog(program));
-      }
+    Shader fs = gl.createShader(RenderingContext.FRAGMENT_SHADER);
+    gl.shaderSource(fs, fragment);
+    gl.compileShader(fs);
+
+    Program program = gl.createProgram();
+    gl.attachShader(program, vs);
+    gl.attachShader(program, fs);
+    gl.linkProgram(program);
+
+    if (!gl.getShaderParameter(vs, RenderingContext.COMPILE_STATUS)) {
+      print(gl.getShaderInfoLog(vs));
+    }
+
+    if (!gl.getShaderParameter(fs, RenderingContext.COMPILE_STATUS)) {
+      print(gl.getShaderInfoLog(fs));
+    }
+
+    if (!gl.getProgramParameter(program, RenderingContext.LINK_STATUS)) {
+      print(gl.getProgramInfoLog(program));
+    }
 
 
-      Float32Array vertices = new Float32Array.fromList(
-          [
-           0.5, 0.5, -0.5, 0.5, 0.5, -0.5, // Triangle 1
-           -0.5, -0.5, -0.5, 0.5, 0.5, -0.5 // Triangle 2
-          ]);
+    Float32List vertices = new Float32List.fromList(
+        [
+         0.5, 0.5, -0.5, 0.5, 0.5, -0.5, // Triangle 1
+         -0.5, -0.5, -0.5, 0.5, 0.5, -0.5 // Triangle 2
+        ]);
 
-      WebGLBuffer squareBuffer = gl.createBuffer();
-      gl.bindBuffer(WebGLRenderingContext.ARRAY_BUFFER, squareBuffer);
-      gl.bufferData(WebGLRenderingContext.ARRAY_BUFFER, vertices, WebGLRenderingContext.STATIC_DRAW);
+    Buffer squareBuffer = gl.createBuffer();
+    gl.bindBuffer(RenderingContext.ARRAY_BUFFER, squareBuffer);
+    gl.bufferData(RenderingContext.ARRAY_BUFFER, vertices, RenderingContext.STATIC_DRAW);
 
-      int itemSize = 2;
-      int numItems = vertices.length ~/ itemSize;
+    int itemSize = 2;
+    int numItems = vertices.length ~/ itemSize;
 
-      gl.useProgram(program);
+    gl.useProgram(program);
 
-      WebGLUniformLocation varColor = gl.getUniformLocation(program, uColor);
-      gl.uniform4fv(varColor, new Float32Array.fromList([0.9, 0.3, 0.0, 0.5]));
+    UniformLocation varColor = gl.getUniformLocation(program, uColor);
+    gl.uniform4fv(varColor, new Float32List.fromList([0.9, 0.3, 0.0, 0.5]));
 
-      WebGLUniformLocation varPosition = gl.getUniformLocation(program, uPosition);
-      gl.uniform3fv(varPosition, new Float32Array.fromList([-1.5, 0, -7]));
+    UniformLocation varPosition = gl.getUniformLocation(program, uPosition);
+    gl.uniform3fv(varPosition, new Float32List.fromList([-1.5, 0.0, -7.0]));
 
-      int aVertextPosition = gl.getAttribLocation(program, attrVertex);
-      gl.enableVertexAttribArray(aVertextPosition);
-      gl.vertexAttribPointer(aVertextPosition, itemSize, WebGLRenderingContext.FLOAT, false, 0, 0);
+    int aVertextPosition = gl.getAttribLocation(program, attrVertex);
+    gl.enableVertexAttribArray(aVertextPosition);
+    gl.vertexAttribPointer(aVertextPosition, itemSize, RenderingContext.FLOAT, false, 0, 0);
 
-      gl.drawArrays(WebGLRenderingContext.TRIANGLES, 0, numItems);
-    });
+    gl.drawArrays(RenderingContext.TRIANGLES, 0, numItems);
   }
 
 }
